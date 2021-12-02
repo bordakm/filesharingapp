40acb9917c05821423885d7c2640c44aaed25fe2
Bordak Mate <mate@webtar.hu>
2021-12-02 14:00:47 +0100

    add angular

---
+++ src/FileHostingAppServer/FileHostingAppServer/Providers/CustomTokenProvider.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileHostingAppServer.Providers
+{
+    public class CustomTokenProvider<TUser> : DataProtectorTokenProvider<TUser> where TUser : class
+    {
+        public CustomTokenProvider(IDataProtectionProvider dataProtectionProvider,
+            IOptions<CustomTokenProviderOptions> options,
+            ILogger<DataProtectorTokenProvider<TUser>> logger)
+        : base(dataProtectionProvider, options, logger)
+        {
+        }
+    }
+
+    public class CustomTokenProviderOptions : DataProtectionTokenProviderOptions
+    {
+    }
+}
