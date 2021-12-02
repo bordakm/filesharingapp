c2c8fb149a787b345c8fed3640c2dda146779808
Bordak Mate <mate@webtar.hu>
2021-12-02 16:01:09 +0100

    sharing

---
+++ src/FileHostingAppServer/FileHostingAppServer.DAL/Entities/SharedFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHostingAppServer.DAL.Entities
+{
+    public class SharedFile
+    {
+        public string OwnerUserId { get; set; }
+        public bool Public { get; set; }
+        public IEnumerable<Share> Shares { get; set; }
+    }
+}
---
+++ src/FileHostingAppServer/FileHostintAppServer.BLL/Services/FileSharingService.cs
@@ -0,0 +1,30 @@
+﻿using FileHostingAppServer.DAL.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileHostingAppServer.BLL.Services
+{
+    public class FileSharingService
+    {
+        private readonly FileHostingDbContext _dbContext;
+
+        public FileSharingService(FileHostingDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task ShareFileWithUserAsync(CancellationToken cancellationToken)
+        {
+            return null;
+        }
+
+        public Task MakeFilePublicAsync(CancellationToken cancellationToken)
+        {
+            return null;
+        }
+    }
+}
